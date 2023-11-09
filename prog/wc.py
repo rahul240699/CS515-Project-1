088d2246d06ded5da65f40be9708501a0296b8dd
Rahul Sohandani <rahulsohandani@Rahuls-MacBook-Air.local>
2023-11-08 23:17:13 -0500

    Added-basic-flags-without-test

--- prog/wc.py
+++ prog/wc.py
@@ -1,6 +1,7 @@
 import argparse
 import subprocess
 import os
+import sys
 
 def word_count_file(filename):
     with open(filename, "r") as file:
@@ -21,16 +22,39 @@ def main():
     #parser.print_help()
 
     parser.add_argument("filename", type = str, help = "Enter the name of the file")
+    parser.add_argument("-l", "--lines", action = "store_true", help = "Count only the total number of lines.")
+    parser.add_argument("-w", "--words", action = "store_true", help = "Count only the total number of words.")
+    parser.add_argument("-c", "--characters", action = "store_true", help = "Count only the total number of characters.")
 
 
     args = parser.parse_args()
 
-    if args.filename != None:
-        if not os.path.isfile(args.filename):
-            print(args.filename+": No such file or directory")
-        else:
-            lines, words, chars = word_count_file(filename = args.filename)
-            print(f"{lines} {words} {chars} {args.filename}")
+    try:
+        # if args.filename is None:
+        #     raise ValueError(args.filename+": ")
+        # else:
+            if not os.path.isfile(args.filename):
+                raise ValueError(args.filename+": No such file or directory")
+            else:
+                lines, words, chars = word_count_file(filename = args.filename)
+                if args.lines and args.words:
+                    print(f"{lines} {words} {args.filename}")
+                elif args.lines and args.characters:
+                    print(f"{lines} {chars} {args.filename}")
+                elif args.characters and args.words:
+                    print(f"{words} {chars} {args.filename}")
+                elif args.lines:
+                    print(f"{lines} {args.filename}")
+                elif args.words:
+                    print(f"{words} {args.filename}")
+                elif args.characters:
+                    print(f"{chars} {args.filename}")
+                else:
+                    print(f"{lines} {words} {chars} {args.filename}")
+    except Exception as e:
+        print(e)
+        sys.exit(1)
+    
 
 if __name__ == "__main__":
     main()
