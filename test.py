da025c27389146902bb241cd235b3f25cfdfea3d
Rahul Sohandani <rahulsohandani@Rahuls-MacBook-Air.local>
2023-11-08 19:52:50 -0500

    first-actual-test

--- test.py
+++ test.py
@@ -1,5 +1,6 @@
 import unittest
 import os
+import subprocess
 #print("Hello World!")
 
 #tests with pytest.py
@@ -10,6 +11,18 @@ def inc(x):
 def test_answer():
     assert inc(3) == 4
 
+test_dir = "/test"
+files = os.listdir(test_dir)
 
-# def test_1wc():
-#     pass
+
+def test_1wc():
+    input_files = [f for f in files if f.endswith(".in")]
+    for i in input_files:
+        result = subprocess.run(["python", "/prog/wc.py", test_dir+"/"+i], stdout=subprocess.PIPE, text = True) 
+        output = result.stdout
+        output_file = i[:-2] + "out"
+        if os.path.isfile(test_dir+"/"+output_file):
+            expected_output = ""
+            with open(test_dir+"/"+output_file) as std:
+                expected_output = std.read()
+            assert expected_output == output
