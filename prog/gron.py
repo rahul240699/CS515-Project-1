f83c3981a760f7d16435a4410618c4e048fe08ff
Rahul Sohandani <rahulsohandani@Rahuls-MacBook-Air.local>
2023-11-09 19:53:45 -0500

    Added-obj-flag-for-gron

--- prog/gron.py
+++ prog/gron.py
@@ -2,21 +2,21 @@ import argparse
 import json
 import os
 
-def gron(data, parent = "", open_dict = True):
+def gron(data, parent = "", open_dict = True, obj = "json"):
     if isinstance(data, dict):
         if not parent:
-            print(f"json = {{}};")
+            print(f"{obj} = {{}};")
         if open_dict and parent:
-            print(f"json.{parent} = {{}};")
+            print(f"{obj}.{parent} = {{}};")
         for k, v in data.items():
             new_parent = f"{parent}.{k}" if parent else k
-            gron(v, new_parent, isinstance(v, dict))
+            gron(v, new_parent, isinstance(v, dict), obj)
     elif isinstance(data, list):
         for i, item in enumerate(data):
             new_parent = f"{parent}[{i}]"
-            gron(item, new_parent, isinstance(item, dict))
+            gron(item, new_parent, isinstance(item, dict), obj)
     else:
-        print(f"json.{parent} = {json.dumps(data)};")
+        print(f"{obj}.{parent} = {json.dumps(data)};")
 
 
 
@@ -24,12 +24,15 @@ def main():
     parser = argparse.ArgumentParser(prog = "Gron Functionality", description = "Flattens JSON.")
 
     parser.add_argument("filename", type = str, help = "Enter the name of the file.")
+    parser.add_argument("--obj", type= str, default="json", help="Print out a different name of the Object")
+    
 
     args = parser.parse_args()
+    # print(args.obj)
     
     f = open(args.filename)
     data = json.load(f)
-    gron(data)
+    gron(data, obj = args.obj)
 
 if __name__ == "__main__":
     main()
