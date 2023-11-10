4fa8551194449bb7a02e572eacad1ac63e715b36
Rahul Sohandani <rahulsohandani@Rahuls-MacBook-Air.local>
2023-11-09 21:31:29 -0500

    Added-obj-flag-for-gron-1

--- prog/gron.py
+++ prog/gron.py
@@ -2,7 +2,7 @@ import argparse
 import json
 import os
 
-def gron(data, parent = "", open_dict = True, obj = "json"):
+def gron(data, parent = "", open_dict = True, open_list = True, obj = "json"):
     if isinstance(data, dict):
         if not parent:
             print(f"{obj} = {{}};")
@@ -10,11 +10,13 @@ def gron(data, parent = "", open_dict = True, obj = "json"):
             print(f"{obj}.{parent} = {{}};")
         for k, v in data.items():
             new_parent = f"{parent}.{k}" if parent else k
-            gron(v, new_parent, isinstance(v, dict), obj)
+            gron(v, new_parent, isinstance(v, dict), isinstance(v,list), obj)
     elif isinstance(data, list):
+        if open_list:
+            print(f"{obj}.{parent} = []")
         for i, item in enumerate(data):
             new_parent = f"{parent}[{i}]"
-            gron(item, new_parent, isinstance(item, dict), obj)
+            gron(item, new_parent, isinstance(item, dict), isinstance(item, list), obj)
     else:
         print(f"{obj}.{parent} = {json.dumps(data)};")
 
