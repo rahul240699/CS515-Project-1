bf4fff002dce5b4ab8d678eee6ce83fe4ffc739a
Rahul Sohandani <rahulsohandani@Rahuls-MacBook-Air.local>
2023-11-16 15:46:15 -0500

    modified-testing

--- test.py
+++ test.py
@@ -18,18 +18,27 @@ files = os.listdir(test_dir)
 def test_1wc():
     input_files = [f for f in files if f.endswith(".in") and "WC" in f]
     for i in input_files:
-        result = subprocess.run(["python", "prog/wc.py", test_dir+"/"+i], stdout = subprocess.PIPE, stderr = subprocess.PIPE, text = True) 
+        with open(test_dir+"/"+i, "r") as test:
+            command = test.readline().strip().split(" ")
+
+        result = subprocess.run(command, stdout = subprocess.PIPE, stderr = subprocess.PIPE, text = True) 
         output = result.stdout
         error = result.stderr
         exit_code = result.returncode
-        print(exit_code)
-        print(error)
+        
         output_file = i[:-2] + "out"
         if os.path.isfile(test_dir+"/"+output_file):
             expected_output = ""
             with open(test_dir+"/"+output_file) as std:
                 expected_output = std.read()
             assert expected_output.strip() == output.strip()
+        
+        status_file = i[:-2] + "status"
+        if os.path.isfile(test_dir+"/"+status_file):
+            expected_exit_code = ""
+            with open(test_dir+"/"+status_file) as ste:
+                expected_exit_code = ste.read()
+            assert expected_exit_code.strip() == exit_code.strip()
 
 def test_1gron():
     input_files = [f for f in files if f.endswith(".in") and "GRON" in f]
