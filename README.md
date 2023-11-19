5496c8c5488c61ea658b08ab90ef36c605e07082
Rahul Sohandani <rahulsohandani@Rahuls-MacBook-Air.local>
2023-11-19 18:09:02 -0500

    readme-2

--- README.md
+++ README.md
@@ -1,16 +1,21 @@
-CSS515Project1
+# CS 515 Project 1
 
+## Author Information
 Rahul Sohandani rsohanda@stevens.edu
 
-https://github.com/rahul240699/CS515-Project-1
+[Github Repository](https://github.com/rahul240699/CS515-Project-1)
 
-#Estimation of Hours Spent on the project
+## Estimation of Hours Spent on the project
 1. I spent around 11 hours approximately on the project.
 
-#Testing of the code
+## Testing of the code
 1. Initially I started the testing on my PC's terminal.
 2. Later on I wrote test cases in the format specified in description, and used the test harness to run the tests and capturing the output, to compare with the expected out in the corresponding .out files for the test.
 
-#Unresolved Bugs or Issues
+## Unresolved Bugs or Issues
 1. It was working fine earlier, but now I am facing an issue where I am pushing my code on github for the github actions, github is unable to capture output from the subprocess command i.e, the stdout from command line, therefore, however it works fine on my local pc, so I believe, it is an environment related issue.
+2. While running test harness to take arguments from the args file the harness is unable to capture the output for the arguments. The program works when you pass arguments on command line itself.
 
+
+
+## Examples of difficult bugs resolved.
--- test.py
+++ test.py
@@ -16,12 +16,14 @@ def run_test(program, test_name):
     input_file = f"test/{program}.{test_name}.in"
     expected_output_file = f"test/{program}.{test_name}.out"
     arg_expected_output_file = f"test/{program}.{test_name}.arg.out"
-    args_file = f"test/{test_name}.args"
+    args_file = f"test/{program}.{test_name}.args"
 
     try:
         
         # print("The name of the program:" +program)
         process = subprocess.run(["python3 prog/"+program+".py "+ input_file], capture_output= True, shell= True, text=True)
+
+        # process = subprocess.Popen(["python3", "prog/"+program+".py", input_file], stdout=subprocess.PIPE, stderr=subprocess.PIPE)
         # process = subprocess.run(["python3", "prog/"+program+".py ",input_file], stdin=subprocess.PIPE, stderr=subprocess.PIPE, text=True)
 
         output = process.stdout
@@ -36,10 +38,14 @@ def run_test(program, test_name):
        
         if os.path.exists(args_file):
             with open(args_file, "r") as file:
-                args = file.read().split()
-            process_args = [f"python prog/{program}.py"] + args
-            process = subprocess.run(process_args, capture_output= True, shell= True, text=True)
-            # process = subprocess.run(process_args, stdin=subprocess.PIPE, stderr=subprocess.PIPE, text=True)
+                args = file.read().split(" ")
+            process_args = [f"python prog/{program}.py " +input_file]
+            for i in args:
+                process_args[0] += " "+i
+            # process = subprocess.run(process_args, capture_output= True, shell= True, text=True)
+            # process = subprocess.run(process_args, stdin=subprocess.PIPE, stderr=subprocess.PIPE, shell= True, text=True)
+
+            # print("The output:" + process.stdout.strip()+ " The INPUT: "+ str(process_args))
            
             if os.path.exists(arg_expected_output_file):
                 with open(arg_expected_output_file, "r") as file:
@@ -72,15 +78,15 @@ def run_tests(program):
     for test_file in os.listdir("test"):
         #print(test_file)
         if test_file.endswith(f".in") and program in test_file:
-            print(test_file)
+            # print(test_file)
             test_name = test_file.split(".")[1]
-            print(test_name)
+            # print(test_name)
             test_count += 1
             if run_test(program, test_name):
                 pass_count += 1
 
     print("\nOK:", pass_count)
-    print("output mismatch:", test_count - pass_count)
+    print("Output mismatch:", test_count - pass_count)
     print("total:", test_count)
 
 if __name__ == "__main__":
