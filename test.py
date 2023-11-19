5aa711ba37d74304da8bdc02e59bc4724e3e3a38
Rahul Sohandani <rahulsohandani@Rahuls-MacBook-Air.local>
2023-11-18 19:25:36 -0500

    testing-new-testing-17

--- test.py
+++ test.py
@@ -21,8 +21,8 @@ def run_test(program, test_name):
     try:
         
         # print("The name of the program:" +program)
-        #process = subprocess.run(["python3 prog/"+program+".py "+ input_file], capture_output= True, shell= True, text=True)
-        process = subprocess.run(["python", "prog/"+program+".py ",input_file], stdin=subprocess.PIPE, stderr=subprocess.PIPE, text=True)
+        process = subprocess.run(["python3 prog/"+program+".py "+ input_file], capture_output= True, shell= True, text=True)
+        # process = subprocess.run(["python3", "prog/"+program+".py ",input_file], stdin=subprocess.PIPE, stderr=subprocess.PIPE, text=True)
 
         output = process.stdout
         # print("This is the output:"+output)
@@ -38,8 +38,8 @@ def run_test(program, test_name):
             with open(args_file, "r") as file:
                 args = file.read().split()
             process_args = [f"python prog/{program}.py"] + args
-            #process = subprocess.run(process_args, capture_output= True, shell= True, text=True)
-            process = subprocess.run(process_args, stdin=subprocess.PIPE, stderr=subprocess.PIPE, text=True)
+            process = subprocess.run(process_args, capture_output= True, shell= True, text=True)
+            # process = subprocess.run(process_args, stdin=subprocess.PIPE, stderr=subprocess.PIPE, text=True)
            
             if os.path.exists(arg_expected_output_file):
                 with open(arg_expected_output_file, "r") as file:
